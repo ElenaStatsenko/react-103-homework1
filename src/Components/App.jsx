7382b238550a4a6ff57d0055b994e1be12d7f740
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2024-06-15 17:42:20 +0300

    сделала разметку

---
+++ src/Components/App.jsx
@@ -0,0 +1,14 @@
+
+import Profile from "./Profile/Profile";
+import userData from './data/userData.json';
+import friends from './data/friends.json';
+import FriendList from "./FriendList/FriendList";
+import transactions from './data/transactions.json';
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
+export default function App() {
+    return (<>
+      <Profile userName={userData}/>
+      <FriendList friends={friends}/>
+      <TransactionHistory items={transactions}/>
+      </>);
+  }
---
+++ src/Components/FriendList/FriendList.jsx
@@ -0,0 +1,12 @@
+import FriendListItem from "../FriendListItem/FriendListItem"
+export default function FriendList({friends}) {
+    return (
+        <ul> {friends.map(friend=>(
+     
+	<li key={friend.id}>
+		<FriendListItem avatar={friend.avatar} name={friend.name} isOnline={friend.isOnline}/>
+	</li>
+    ))}
+</ul>
+)
+}
