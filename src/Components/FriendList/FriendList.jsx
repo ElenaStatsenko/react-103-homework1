e01224d000513c0166fbe0a319a5c82b0a07b7be
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2024-06-16 22:07:30 +0300

    начала делать стилизацию второй домашки

--- src/Components/FriendList/FriendList.jsx
+++ src/Components/FriendList/FriendList.jsx
@@ -1,9 +1,10 @@
 import FriendListItem from "../FriendListItem/FriendListItem"
+import css from "./FriendList.module.css"
 export default function FriendList({friends}) {
     return (
-        <ul> {friends.map(friend=>(
+        <ul className={css.friendBox}> {friends.map(friend=>(
      
-	<li key={friend.id}>
+	<li className={css.friendCard} key={friend.id} >
 		<FriendListItem avatar={friend.avatar} name={friend.name} isOnline={friend.isOnline}/>
 	</li>
     ))}
--- src/Components/FriendListItem/FriendListItem.jsx
+++ src/Components/FriendListItem/FriendListItem.jsx
@@ -1,9 +1,13 @@
+
+import css from "./FriendListItem.module.css"
 export default function FriendListItem({ avatar, name, isOnline }) {
+ console.log(isOnline);
   return (
     <div>
       <img src={avatar} alt="Avatar" width="48" />
       <p>{name}</p>
-      <p>{isOnline}</p>
+      <p>{"isOnline"}</p>
     </div>
+   
   );
 }
